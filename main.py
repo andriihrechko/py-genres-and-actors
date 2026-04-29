782842c68dc598517bdbf76d3a2f6b67f3349dea
andriihrechko <andriihrechko250703@gmail.com>
2026-04-29 15:29:20 +0300

    refactor: refactor actors list

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ from db.models import Genre, Actor
 def main() -> QuerySet:
     genres = ["Western", "Action", "Dramma"]
     actors = [
-        {"first_name": "George", "last_name": "Klooney"},
-        {"first_name": "Kianu", "last_name": "Reaves"},
-        {"first_name": "Scarlett", "last_name": "Keegan"},
-        {"first_name": "Will", "last_name": "Smith"},
-        {"first_name": "Jaden", "last_name": "Smith"},
-        {"first_name": "Scarlett", "last_name": "Johansson"},
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
     ]
 
-    genres = [Genre(name=item) for item in genres]
-    actors = [Actor(**item) for item in actors]
+    genres = [
+        Genre(name=item) for item in genres
+    ]
+    actors = [
+        Actor(first_name=name, last_name=surname)
+        for name, surname in actors
+    ]
 
     # 1. CREATE
     Genre.objects.bulk_create(genres)
